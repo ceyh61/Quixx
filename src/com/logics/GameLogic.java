17f8084007586f298e86aba98b61c8f262c4fcbc
Rahsch <88612168+Rahsch@users.noreply.github.com>
2023-03-06 16:23:19 +0100

    Add files via upload

---
+++ src/com/logics/GameLogic.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class GameLogic {
+
+    private static final int NUMBER_DICES = 6;
+
+    public GameLogic(){
+
+    }
+    private int getRandomNumberBetweenOneAndSix(){
+        return (int)(Math.random()*6 +1);
+    }
+
+    public ArrayList<Integer> rollDice(int NUMBER_DICES){
+        ArrayList<Integer> diceResults = new ArrayList<Integer>();
+
+        for(int i=0;i<6;i++){
+            diceResults.add(getRandomNumberBetweenOneAndSix());
+        }
+        return diceResults;
+    }
+}
