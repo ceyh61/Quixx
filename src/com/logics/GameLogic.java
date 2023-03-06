aac43a1b02e20108a0d012b696b3b4dc8250e625
Rahsch <88612168+Rahsch@users.noreply.github.com>
2023-03-06 19:19:50 +0100

    Add files via upload

--- src/com/logics/GameLogic.java
+++ src/com/logics/GameLogic.java
@@ -11,11 +11,25 @@ public class GameLogic {
         return (int)(Math.random()*6 +1);
     }
 
-    public ArrayList<Integer> rollDice(int NUMBER_DICES){
+    public ArrayList<Integer> createDiceArrayList(){
         ArrayList<Integer> diceResults = new ArrayList<Integer>();
+        boolean redRow = new Connector().isRedRowComplete();
+        boolean greenRow = new Connector().isYellowRowComplete();
+        boolean yellowRow = new Connector().isGreenRowComplete();
+        boolean blueRow = new Connector().isBlueRowComplete();
 
-        for(int i=0;i<NUMBER_DICES;i++){
-            diceResults.add(getRandomNumberBetweenOneAndSix());
+        for(int i=0;i<NUMBER_DICES;i++) {
+            if (redRow && i == 2) {
+                diceResults.add(0);
+            } else if (yellowRow && i == 3) {
+                diceResults.add(0);
+            } else if (greenRow && i == 4) {
+                diceResults.add(0);
+            } else if (blueRow && i == 5) {
+                diceResults.add(0);
+            } else {
+                diceResults.add(getRandomNumberBetweenOneAndSix());
+            }
         }
         return diceResults;
     }
