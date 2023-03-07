9257da7981e5632ebf0d91a7e5ae91cbfa3160dc
My-DESKTOP <>
2023-03-08 00:00:15 +0100

    new methods added to Connector class

--- src/com/logics/Connector.java
+++ src/com/logics/Connector.java
@@ -4,16 +4,7 @@ import java.util.ArrayList;
 
 public class Connector {
     
-    public boolean redRowComplete;
-    public boolean yellowRowComplete;
-    public boolean greenRowComplete;
-    public boolean blueRowComplete;
-    
     public Connector() {
-        redRowComplete = true;
-        yellowRowComplete = false;
-        greenRowComplete = false;
-        blueRowComplete = true;
     }
 
     //------------------------------------------------------------
@@ -21,9 +12,10 @@ public class Connector {
     //---1. Database methods -------------------------------------
     //---2. Pathfinder methods -----------------------------------
     //---3. Player input methods ---------------------------------
-    //---4. Booking methods --------------------------------------
-    //---5. User methods -----------------------------------
-    //---6. Utility methods --------------------------------------
+    //---4. Dice Read/Write methods ------------------------------
+    //---5. Dice Rolling methods ---------------------------------
+    //---6. Player Data Read/Write methods -----------------------
+    //---7. Calculations methods ---------------------------------
     //------------------------------------------------------------
 
 
@@ -105,6 +97,91 @@ public class Connector {
     public boolean nameCharacterCheck(String playerName) {
         NameValidation nv = new NameValidation();
         boolean b = nv.isName_Character_Valid(playerName);
+        return b;
+    }
+*/
+    // This method checks if a player name is between 1-10 characters
+    // and checks if a players name contains only letter 
+    public String validateName(String fileName) {
+        String answer = "";
+        Pathfinder pf = new Pathfinder();
+        answer = pf.getLastSavedFilePath(fileName);
+        return answer;
+    }
+
+    //------------------------------------------------------------
+    //---4.---------------Dice Read/Write methods-----------------
+    //------------------------------------------------------------
+
+    public String getLastDiceData() {
+        String answer = "";
+        DiceDataRW dd = new DiceDataRW();
+        answer = dd.readDiceFile();
+        return answer;
+    }
+
+    public void saveDiceData(String newDiceData) {
+        DiceDataRW dd = new DiceDataRW();
+        dd.writeToDiceFile(newDiceData);
+    }
+
+    //------------------------------------------------------------
+    //---5.---------------Dice Rolling methods--------------------
+    //------------------------------------------------------------
+
+    public String getDiceRollingResult() {
+        String answer = "";
+        DiceRolls dr = new DiceRolls();
+        answer = dr.rollTheDices();
+        return answer;
+    }
+
+    //------------------------------------------------------------
+    //---6.------------Player Data Read/Write methods-------------
+    //------------------------------------------------------------
+
+    // This method gets the (file name, color type and chosen number)
+    // returns the number as a String
+    public String get_A_Number(String fileName, String color, String number) {
+        String answer = "";
+        PlayerDataRW pd = new PlayerDataRW();
+        answer = pd.readOnlyOneNumber(fileName, color, number);
+        return answer;
+    }
+
+    // This method gets the file name and color and returns all numbers of that color
+    public ArrayList<String> get_A_Line(String fileName, String color) {
+        ArrayList<String> answer = new ArrayList<>();
+        PlayerDataRW pd = new PlayerDataRW();
+        answer = pd.readOnlyOneLine(fileName, color);
+        return answer;
+    }
+
+    public boolean isNumberCrossed(String fileName, String color, String number) {
+        boolean b = false;
+        PlayerDataRW pd = new PlayerDataRW();
+        b = pd.checkIfNumberIsCrossed(fileName, color, number);
+        return b;
+    }
+
+    //------------------------------------------------------------
+    //---7.------------Calculations methods-----------------------
+    //------------------------------------------------------------
+
+    public boolean isRedRowComplete() {
+        boolean b = false;
+        return b;
+    }
+    public boolean isYellowRowComplete() {
+        boolean b = false;
+        return b;
+    }
+    public boolean isGreenRowComplete() {
+        boolean b = false;
+        return b;
+    }
+    public boolean isBlueRowComplete() {
+        boolean b = false;
         return b;
     }
 
