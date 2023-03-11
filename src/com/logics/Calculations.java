cce6f5c54f568c6bb0546c143acd51c460a84ac5
ceyh61 <x61ceyhun61x@gmail.com>
2023-03-11 21:07:42 +0100

    Modified Methods in Calculations

--- src/com/logics/Calculations.java
+++ src/com/logics/Calculations.java
@@ -6,86 +6,95 @@ import java.util.Arrays;
 public class Calculations {
 
     private static final String[] POINTS_ARRAY = {"0", "1", "3", "6", "10", "15", "21", "28", "36", "45", "55", "66", "78"};
-    
+
     public Calculations(){
 
     }
 
-    protected int calculateRedPoints(ArrayList<String> redRow){
+    protected int calculateRedPoints(String fileName){
         int points = 0;
         int countCross = 0;
 
+        Connector cn = new Connector();
+        ArrayList <String> redRow = cn.get_A_Line(fileName, "red");
         ArrayList <String> pointList = new ArrayList<>();
         // adding all the point from a normal Array to an ArrayList for simple calculations
         pointList.addAll(Arrays.asList(POINTS_ARRAY));
 
-        // counts the number of the crossed numbers 
+        // counts the number of the crossed numbers
         for(int i = 0 ; i < redRow.size() ; i++){
             if (redRow.get(i).contains("X")){
                 countCross++;
             }
         }
-        // finds the corresponding point from the list and returns it. 
+        // finds the corresponding point from the list and returns it.
         points = Integer.parseInt(pointList.get(countCross));
 
         return points;
     }
 
-    protected int calculateYellowPoints(ArrayList<String> yellowRow){
+    protected int calculateYellowPoints(String fileName){
         int points = 0;
         int countCross = 0;
 
+
+        Connector cn = new Connector();
+        ArrayList <String> yellowRow = cn.get_A_Line(fileName, "yel");
         ArrayList <String> pointList = new ArrayList<>();
         // adding all the point from a normal Array to an ArrayList for simple calculations
         pointList.addAll(Arrays.asList(POINTS_ARRAY));
 
-        // counts the number of the crossed numbers 
+        // counts the number of the crossed numbers
         for(int i = 0 ; i < yellowRow.size() ; i++){
             if (yellowRow.get(i).contains("X")){
                 countCross++;
             }
         }
-        // finds the corresponding point from the list and returns it. 
+        // finds the corresponding point from the list and returns it.
         points = Integer.parseInt(pointList.get(countCross));
 
         return points;
     }
 
-    protected int calculateGreenPoints(ArrayList<String> greenRow){
+    protected int calculateGreenPoints(String fileName){
         int points = 0;
         int countCross = 0;
 
+        Connector cn = new Connector();
+        ArrayList <String> greenRow = cn.get_A_Line(fileName, "gre");
         ArrayList <String> pointList = new ArrayList<>();
         // adding all the point from a normal Array to an ArrayList for simple calculations
         pointList.addAll(Arrays.asList(POINTS_ARRAY));
 
-        // counts the number of the crossed numbers 
+        // counts the number of the crossed numbers
         for(int i = 0 ; i < greenRow.size() ; i++){
             if (greenRow.get(i).contains("X")){
                 countCross++;
             }
         }
-        // finds the corresponding point from the list and returns it. 
+        // finds the corresponding point from the list and returns it.
         points = Integer.parseInt(pointList.get(countCross));
 
         return points;
     }
 
-    protected int calculateBluePoints(ArrayList<String> blueRow){
+    protected int calculateBluePoints(String fileName){
         int points = 0;
         int countCross = 0;
 
+        Connector cn = new Connector();
+        ArrayList <String> blueRow = cn.get_A_Line(fileName, "blu");
         ArrayList <String> pointList = new ArrayList<>();
         // adding all the point from a normal Array to an ArrayList for simple calculations
         pointList.addAll(Arrays.asList(POINTS_ARRAY));
 
-        // counts the number of the crossed numbers 
+        // counts the number of the crossed numbers
         for(int i = 0 ; i < blueRow.size() ; i++){
             if (blueRow.get(i).contains("X")){
                 countCross++;
             }
         }
-        // finds the corresponding point from the list and returns it. 
+        // finds the corresponding point from the list and returns it.
         points = Integer.parseInt(pointList.get(countCross));
 
         return points;
@@ -100,30 +109,75 @@ public class Calculations {
         penaltyList = cnn.getPenaltyArray(fileName);
         for (int i = 0; i < penaltyList.size(); i++) {
             if(penaltyList.get(i).equalsIgnoreCase("Y")) {
-                points++;
+                points-= 5;
             }
         }
         return points;
     }
 
-    protected int calculateAllPointsOfPlayer(ArrayList<String> redRow,ArrayList<String> yellowRow,ArrayList<String> greenRow,ArrayList<String> blueRow){
-        int redPoints = calculateRedPoints(redRow);
-        int yellowPoints = calculateYellowPoints(yellowRow);
-        int greenPoints = calculateGreenPoints(greenRow);
-        int bluePoints = calculateBluePoints(blueRow);
-        int penaltyPoints = calculatePenaltiesPoints(redRow,yellowRow,greenRow,blueRow);
+    public int calculateAllPointsOfPlayer(String fileName){
+        int redPoints = calculateRedPoints(fileName);
+        System.out.println(redPoints);
+        int yellowPoints = calculateYellowPoints(fileName);
+        int greenPoints = calculateGreenPoints(fileName);
+        int bluePoints = calculateBluePoints(fileName);
+        int penaltyPoints = calculatePenaltiesPoints(fileName);
         int playersPoints = redPoints+yellowPoints+greenPoints+bluePoints+penaltyPoints;
 
         return playersPoints;
     }
-    protected boolean checkIfAnyPlayerHasFourPenalties(){
+    public boolean checkIfAnyPlayerHasFourPenalties(int playersCount){
         boolean check = false;
-
+        ArrayList <String> penaltyList = new ArrayList<>();
+        int counter = 0;
+        for (int i = 0; i < playersCount; i++) {
+            String fileName = "p" + (i+1);
+            Connector cn = new Connector();
+            penaltyList = cn.getPenaltyArray(fileName);
+            for (int j = 0; j < penaltyList.size(); j++) {
+                if(penaltyList.get(j).equalsIgnoreCase("Y")) {
+                    counter++;
+                    System.out.println(fileName + "Ja");
+                }
+            }
+            System.out.println(counter);
+            if (counter ==4){
+                check = true;
+                break;
+            }
+            counter = 0;
+        }
 
         return check;
     }
-    protected boolean checkIfTwoRowsAreDisabled(){
+    public boolean checkIfTwoRowsAreDisabled(int playersCount){
         boolean check = false;
+        int counter = 0;
+        for (int i = 0; i < playersCount; i++) {
+            String fileName = "p" + (i + 1);
+            Connector cn = new Connector();
+            if (cn.isNumberCrossed(fileName, "red", "12")) {
+                counter++;
+            }
+
+            if (cn.isNumberCrossed(fileName, "yel", "12")) {
+                counter++;
+            }
+
+            if (cn.isNumberCrossed(fileName, "gre", "2")) {
+                counter++;
+            }
+
+            if (cn.isNumberCrossed(fileName, "blu", "2")) {
+                counter++;
+            }
+        }
+
+        System.out.println(counter);
+
+        if (counter>=2){
+            check = true;
+        }
 
         return check;
     }
