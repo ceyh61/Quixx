8b6b9821f17b47029019d81553890df068b7d00a
S-Sediqi <sediqi.samim@fh-swf.de>
2023-03-17 20:05:27 +0100

    getActivePlayerName method in Game Process added

--- src/com/UI/ActivePlayer.java
+++ src/com/UI/ActivePlayer.java
@@ -100,11 +100,11 @@ public class ActivePlayer extends JFrame implements ActionListener {
         pName_Lb.setHorizontalAlignment(SwingConstants.CENTER);
         c.add(pName_Lb);
 
-        String wwuerfel = cnn.getLastDiceData();
-        String[] ergebnis = wwuerfel.split(",");
+        String wuerfel = cnn.getLastDiceData();
+        String[] result = wuerfel.split(",");
 
         // create the first white dice label
-        wDice1_Lb = new JLabel(ergebnis[0]);
+        wDice1_Lb = new JLabel(result[0]);
         wDice1_Lb.setOpaque(true);
         wDice1_Lb.setBackground(Color.white);
         wDice1_Lb.setFont(new Font("Arial", Font.BOLD, 18));
@@ -113,7 +113,7 @@ public class ActivePlayer extends JFrame implements ActionListener {
         wDice1_Lb.setHorizontalAlignment(SwingConstants.CENTER);
         c.add(wDice1_Lb);
 
-        wDice2_Lb = new JLabel(ergebnis[1]);
+        wDice2_Lb = new JLabel(result[1]);
         wDice2_Lb.setOpaque(true);
         wDice2_Lb.setBackground(Color.white);
         wDice2_Lb.setFont(new Font("Arial", Font.BOLD, 18));
@@ -122,7 +122,7 @@ public class ActivePlayer extends JFrame implements ActionListener {
         wDice2_Lb.setHorizontalAlignment(SwingConstants.CENTER);
         c.add(wDice2_Lb);
 
-        rDice_Lb = new JLabel(ergebnis[2]);
+        rDice_Lb = new JLabel(result[2]);
         rDice_Lb.setFont(new Font("Arial", Font.BOLD, 18));
         rDice_Lb.setBounds(315, 20, 50, 50);
         rDice_Lb.setOpaque(true);
@@ -131,7 +131,7 @@ public class ActivePlayer extends JFrame implements ActionListener {
         rDice_Lb.setHorizontalAlignment(SwingConstants.CENTER);
         c.add(rDice_Lb);
 
-        yDice_Lb = new JLabel(ergebnis[3]);
+        yDice_Lb = new JLabel(result[3]);
         yDice_Lb.setFont(new Font("Arial", Font.BOLD, 18));
         yDice_Lb.setBounds(380, 20, 50, 50);
         yDice_Lb.setOpaque(true);
@@ -140,7 +140,7 @@ public class ActivePlayer extends JFrame implements ActionListener {
         yDice_Lb.setHorizontalAlignment(SwingConstants.CENTER);
         c.add(yDice_Lb);
 
-        gDice_Lb = new JLabel(ergebnis[4]);
+        gDice_Lb = new JLabel(result[4]);
         gDice_Lb.setFont(new Font("Arial", Font.BOLD, 18));
         gDice_Lb.setBounds(445, 20, 50, 50);
         gDice_Lb.setOpaque(true);
@@ -149,7 +149,7 @@ public class ActivePlayer extends JFrame implements ActionListener {
         gDice_Lb.setHorizontalAlignment(SwingConstants.CENTER);
         c.add(gDice_Lb);
 
-        bDice_Lb = new JLabel(ergebnis[5]);
+        bDice_Lb = new JLabel(result[5]);
         bDice_Lb.setFont(new Font("Arial", Font.BOLD, 18));
         bDice_Lb.setBounds(510, 20, 50, 50);
         bDice_Lb.setOpaque(true);
@@ -225,8 +225,8 @@ public class ActivePlayer extends JFrame implements ActionListener {
         wNumber_Lb.setForeground(Color.BLACK);
         c.add(wNumber_Lb);
 
-        int zahl1 = Integer.parseInt(ergebnis[0]);
-        int zahl2 = Integer.parseInt(ergebnis[1]);
+        int zahl1 = Integer.parseInt(result[0]);
+        int zahl2 = Integer.parseInt(result[1]);
         addierteZahl = zahl1+zahl2;
         wSumNumber_Lb = new JLabel("" + addierteZahl);
         wSumNumber_Lb.setFont(new Font("Arial", Font.BOLD, 14));
